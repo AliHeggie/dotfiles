27b1b28196357a0858dedccb230d9cc73e4d2718
AliHeggie <alastair.heggie@gmail.com>
2020-04-25 17:52:04 +0100

    Fix install to create symlinks properly

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 mv ~/.bashrc ~/.bashrc.bak
 mv ~/.tmux.conf ~/.tmux.conf.bak
-mv ~/vimrc ~/vimrc.bak
+mv ~/.vimrc ~/.vimrc.bak
+mv ~/.gitconfig ~/.gitconfig.bak
 
 ln -sc "~/dotfiles/.bashrc" ~
 ln -sc "~/dotfiles/.tmux.conf" ~
-ln -sc "~/dotfiles/vimrc" ~
+ln -sc "~/dotfiles/.vimrc" ~
 ln -sc "~/dotfiles/.gitconfig" ~
 
